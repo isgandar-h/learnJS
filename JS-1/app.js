c68db8ecc832da9535f89fcf0bec72af0229d8d1
isgandar-h <i.hasilov277@gmail.com>
2025-03-08 05:15:45 -0800

    practice JS

---
+++ JS-1/app.js
@@ -0,0 +1,4 @@
+// console.log('Hello from app.js');
+
+
+// docment.getElementById('app').innerHTML = 'Hello from app.js';
---
+++ JS-2/app.js
@@ -0,0 +1,20 @@
+// Scope
+
+// 1. Global Scope
+// 2. Function Scope
+// 3. Block Scope
+// 4. Lexical Scope
+// 5. Module Scope
+// 6. Lexical Environment
+// 7. Scope Chain
+// 8. Hoisting
+// 9. Closure
+// 10. Garbage Collection
+// 11. Memory Leak
+// 12. Temporal Dead Zone
+// 13. Call Stack
+// 14. Event Loop
+// 15. Web API
+// 16. Callback Queue
+// 17. Microtask Queue
+// 18. Render Queue
