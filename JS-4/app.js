ca5f93c01e4979a6539845876d040c60cd9e03a8
isgandar-h <i.hasilov277@gmail.com>
2025-03-09 03:34:07 -0700

    practice JS

---
+++ JS-4/app.js
@@ -0,0 +1,21 @@
+/*
+1-String
+2-Number
+3-Boolean
+4-Null
+5-Undefined
+6-Object
+7-Function
+*/
+
+let myString = "Hello World";
+let myNumber = 123;
+let myBoolean = true;
+let myNull = null;
+let myUndefined = undefined;
+let myObject = {};
+let myFunction = function() {};
+
+
+let test = ;
+console.log(typeof test);
